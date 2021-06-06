da47def008feeb9ac5c8f69c3545726dc9a9b18e
cjmunoz4 <cjmunoz4@uc.cl>
2021-06-06 13:20:16 -0400

    cat fetch

--- vue-grupo-3/src/main.js
+++ vue-grupo-3/src/main.js
@@ -6,3 +6,6 @@ const app = createApp(App)
 app.use(vuetify)
 
 app.mount('#app')
+
+
+fetch("https://api.thecatapi.com/v1/images/search", {headers: {"x-api-key" : "297ece98-a6ac-419d-9d86-fbe4c3ccdeac"}}).then((response)=> {console.log(response)}) 
